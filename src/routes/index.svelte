bea9b183b857dd8edec087e01835616b818f369a
amy stout <asbilly92@gmail.com>
2021-12-13 15:57:49 -0500

    changed zIndex on hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
 
 <Header />
 
-<div class="p-10">
+<div class="p-10 negative">
 	<Hero />
 </div>
 
@@ -335,6 +335,17 @@
 	#outside-circle,
 	#matts-marine {
 		fill-opacity: 0.08;
+	}
+
+	.negative {
+		z-index: -10;
+	}
+
+	#services {
+		background-color: #dbeafe;
+		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 2 1'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%23DBEAFE'/%3E%3Cstop offset='1' stop-color='%23B8EAFF'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='0' y2='1'%3E%3Cstop offset='0' stop-color='%23B5C2FF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23B5C2FF' stop-opacity='1'/%3E%3C/linearGradient%3E%3ClinearGradient id='c' gradientUnits='userSpaceOnUse' x1='0' y1='0' x2='2' y2='2'%3E%3Cstop offset='0' stop-color='%23B5C2FF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23B5C2FF' stop-opacity='1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect x='0' y='0' fill='url(%23a)' width='2' height='1'/%3E%3Cg fill-opacity='0.5'%3E%3Cpolygon fill='url(%23b)' points='0 1 0 0 2 0'/%3E%3Cpolygon fill='url(%23c)' points='2 1 2 0 0 0'/%3E%3C/g%3E%3C/svg%3E");
+		background-attachment: fixed;
+		background-size: cover;
 	}
 
 	.bg-graphic {
