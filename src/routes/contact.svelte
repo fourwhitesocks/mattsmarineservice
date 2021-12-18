de4b9540a1688f0667ae9f124153ef28748fac9f
amy stout <asbilly92@gmail.com>
2021-12-18 11:34:15 -0500

    feat: :bug: added prerender to contact form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,5 +1,5 @@
-<script>
-	//import Header from 'incorrectly-named-bu/header.svelte';
+<script context="module">
+	export const prerender = true;
 </script>
 
 <svelte:head>
@@ -8,24 +8,51 @@
 </svelte:head>
 
 <div>
-	<div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-		<form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-			<input type="hidden" name="form-name" value="contact" />
-			<p>
-				<label for="name">Name</label>
-				<input type="text" id="name" name="name" />
-			</p>
-			<p>
-				<label for="email">Email</label>
-				<input type="text" id="email" name="email" />
-			</p>
-			<p>
-				<label for="message">Message</label>
-				<textarea id="message" name="message" />
-			</p>
-			<p>
-				<button type="submit">Send</button>
-			</p>
-		</form>
+	<div class="w-full  max-w-6xl mx-auto pt-16 pb-32">
+		<h1
+			class="text-4xl tracking-tight font-extrabold text-blue-900 sm:text-5xl md:text-6xl s-Zp10jLLCVhnZ"
+		>
+			Welcome! Questions?
+		</h1>
+
+		<p
+			class="pt-16 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+		>
+			Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit
+			sunt amet fugiat veniam occaecat fugiat aliqua.
+		</p>
+
+		<section class="pt-16 pb-32">
+			<form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+				<input type="hidden" name="form-name" value="contact" />
+				<p class="pt-8">
+					<label for="name">Name</label>
+					<input type="text" id="name" name="name" />
+				</p>
+				<p class="pt-8">
+					<label for="email">Email</label>
+					<input type="text" id="email" name="email" />
+				</p>
+				<p class="pt-8">
+					<label for="message">Message</label>
+					<textarea id="message" name="message" />
+				</p>
+				<p class="pt-8">
+					<button
+						type="submit"
+						class="py-2 px-3 text-white bg-blue-900 hover:bg-blue-800 hover:no-underline rounded transition duration-300"
+						>Send</button
+					>
+				</p>
+			</form>
+		</section>
 	</div>
 </div>
+
+<style>
+	input,
+	textarea {
+		border: 1px solid #dedede;
+		line-height: 3;
+	}
+</style>
