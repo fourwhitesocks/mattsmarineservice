b35151f8c76fc722d18f33e8f12168290cf8315c
amy stout <asbilly92@gmail.com>
2021-12-20 14:26:07 -0500

    fix:

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-/** @type {import('@sveltejs/kit').Config} */
-const adapter = require('@sveltejs/adapter-netlify');
-module.exports = {
-
+/**@type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify'
+const config = {
 	kit: {
-		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		prerender: {
-			crawl: true,
-			enabled: true,
-			force: true,
-			pages: ['*'],
-		},
-	},
+		adapter: adapter()
+	}
 };
+
+export default config;
