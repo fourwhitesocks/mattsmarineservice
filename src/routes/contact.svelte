74ab218296e179179609ac96e8a9a2a03d5f1c37
amy stout <asbilly92@gmail.com>
2021-12-20 18:12:30 -0500

    style: :lipstick: fixed container on contact form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -16,7 +16,7 @@
 	<title>Matts Marine Services | Monroe, Mi | Contact</title>
 </svelte:head>
 
-<div>
+<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center">
 	<div class="p-10 max-6xl mx-auto">
 		<h1
 			class="text-4xl tracking-tight font-extrabold text-blue-900 sm:text-5xl md:text-6xl s-Zp10jLLCVhnZ"
